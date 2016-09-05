306b2721983e00837fd15ef72af8df11b7f6ba0f
Nicholas Skinsacos <nskins@umich.edu>
2016-09-05 17:02:35 -0400

    Adds a script which removes the preset files

---
+++ scripts/clear_preset.sh
@@ -0,0 +1,35 @@
+# Removes 'Battle' preset data.
+
+rm -f ../src/Battle/BattleCommand/escape.rb
+rm -f ../src/Battle/BattleCommand/Attack/kick.rb
+rm -f ../src/Battle/BattleCommand/Attack/smash.rb
+
+# Removes 'Entity' preset data.
+
+rm -f ../src/Entity/Monster/alien.rb
+
+# Removes 'Event' preset data.
+
+rm -f ../src/Event/box.rb
+rm -f ../src/Event/Shop/bakery.rb
+
+# Removes 'Item' preset data.
+
+rm -f ../src/Item/Food/donut.rb
+rm -f ../src/Item/Equippable/Weapon/baguette.rb
+
+# Removes 'Map' preset data.
+
+rm -f ../src/Map/Map/donut_field.rb
+rm -f ../src/Map/Tile/dirt.rb
+rm -f ../src/Map/Tile/wall.rb
+
+# Removes 'Story' preset data.
+
+rm -f ../src/Story/introduction.rb
+
+# Removes content references in 'driver.rb'.
+# TODO
+
+# Removes content references in 'main.rb'.
+# TODO
