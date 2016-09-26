45d3c13f8eec28a7bb614ad4dd429f81d3280791
Nicholas Skinsacos <nskins@umich.edu>
2016-09-26 09:51:20 -0400

    Provide a more interesting event for hole

--- lib/Event/hole.rb
+++ lib/Event/hole.rb
@@ -2,17 +2,29 @@ require_relative 'event.rb'
 
 # PRESET DATA
 class Hole < Event
-  def initialize(params = {command: "dig"})
+  def initialize(params = {command: "dig",
+                           max_reward: 20})
     super(params)
+    @max_reward = params[max_reward] || 20
+    (@max_reward = 1) if (@max_reward < 1)
   end
 
   def run(player)
     if (player.has_item("Shovel") != -1)
-      # @todo do something interesting for the preset game.
 
-      print "#{player.name} tries digging, but nothing happens.\n\n"
+      reward = Random.rand(0..@max_reward)
+
+      print "#{player.name} starts digging...\n\n"
+      sleep(1)
+      print "...and finds #{reward} gold!\n\n"
+
+      player.gold += reward
+
+      @visible = false
     else
       print "You'll need proper gardening equipment for that.\n\n"
     end
   end
+
+  attr_accessor :max_reward
 end
