6dbfda2da74767bb20ab1a70f7f4c2043a60641c
Nicholas Skinsacos <nskins@umich.edu>
2016-11-10 23:55:52 -0500

    Provide much better example of map creation (Fixes #34)

--- lib/Map/Map/donut_field.rb
+++ lib/Map/Map/donut_field.rb
@@ -12,21 +12,39 @@ require_relative '../../Event/Shop/wacky_clothes.rb'
 class DonutField < Map
   def initialize(params = {})
     @name = "Donut Field"
+    
+    dirt = Dirt.new
+    wall = Wall.new
+    
+    @tiles = Array.new
+  
+    # Set the first row of the map with generic Dirt.
+    @tiles[0] = Array.new
+    3.times { @tiles[0] << dirt.clone }
+    
+    # Add special events to the first row.
+    @tiles[0][2].description = "Dirt surrounds you.\nYou sense a strange presence."
+    @tiles[0][2].monsters = [Alien.new]
+    
+    # Set the second row of the map.
+    @tiles[1] = Array.new
+    @tiles[1] << dirt.clone << wall.clone << dirt.clone
+    
+    # Add special events to the second row.
+    @tiles[1][2].events = [Dan.new]
+    
+    # Set the third row of the map.
+    @tiles[2] = Array.new
+    3.times { @tiles[2] << dirt.clone }
+      
+    # Add special events to the third row.
+    @tiles[2][0].description = "The world-famous \"Bob's Bakery\" stands here."
+    @tiles[2][0].events = [Bakery.new, Hole.new]
+    @tiles[2][1].events = [Box.new(gold: 60)]
+    @tiles[2][2].description = "There is a shop with strange clothes nearby."
+    @tiles[2][2].events = [WackyClothesShop.new]
 
-    @tiles = [
-              [ Dirt.new, Dirt.new,
-                  Dirt.new(description: "Dirt surrounds you.\nYou sense a strange presence.",
-                           monsters: [Alien.new]) ],
-
-              [ Dirt.new, Wall.new, Dirt.new(events: [Dan.new]) ],
-
-              [ Dirt.new(description: "The world-famous \"Bob's Bakery\" stands here.",
-                         events: [Bakery.new, Hole.new]),
-                  Dirt.new(events: [ Box.new(gold: 60) ] ),
-                Dirt.new(description: "There is a shop with strange clothes nearby.",
-                         events: [WackyClothesShop.new]) ]
-             ]
-
+    # Set the location for the character to spawn after death.
     @regen_location = Couple.new(0,0)
   end
 end
