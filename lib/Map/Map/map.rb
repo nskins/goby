d638f28eacde33f73e9d021e33cdd6f790121436
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-11-28 13:27:00 -0800

    Replace new map.rb file with old because of branch issue

--- lib/Map/Map/map.rb
+++ lib/Map/Map/map.rb
@@ -4,29 +4,11 @@ class Map
 	# @option params [String] :name the name.
 	# @option params [[Tile]] :tiles the content of the map.
 	# @option params [Couple(Int,Int)] :regen_location the respawn-on-death coordinates.
-
-################ Added @music attribute and attr_accessor :music #############################
 	def initialize(params = {})
 		@name = params[:name] || "Map"
 		@tiles = params[:tiles] || [ [Tile.new ] ]
 		@regen_location = params[:regen_location] || Couple.new(0,0)
-		@music = true
 	end
-
-################################# Adding this method ####################################
-	def play_music(bool)
-		@music = bool
-		if @music === true
-			$pid = Process.spawn "while true; do afplay intro.mp3; done"
-		elsif @music === false
-			# Use sleep for testing music off.  Music will continue to play until it hits the end of track
-			# sleep(1)
-			Process.kill(15, $pid)
-		else
-			p "Please enter a true or false parameter"
-		end
-	end
-##########################################################################################
 
 	# @param [Map] rhs the map on the right.
 	def ==(rhs)
@@ -34,26 +16,15 @@ class Map
 	end
 
   # Prints the map in a nice format.
-	def display
-    #greeting
-    puts "\nYou're in " + @name + "!\n\n";
-    @tiles.each do |sub|
-
-      #centers map under the greeting
-      for i in 1..(name.length/2)
-        print " "
+	def to_s
+		output = ""
+    @tiles.each do |row|
+      row.each do |tile|
+        output += (tile.graphic + " ")
       end
-      sub.each do |tile|
-        if tile.passable
-          print "·"
-        else
-          print "■"
-        end
-      end
-    puts ""
+			output += "\n"
     end
-		puts "\n· - passable space" +
-         "\n■ - impassable space"
+		return output
   end
 
 	# Returns true when @tiles[y][x] is an existing index of @tiles.
@@ -66,11 +37,6 @@ class Map
 		return (y >= 0 && y < @tiles.length && x >= 0 && x < @tiles[y].length)
 	end
 
-	attr_accessor :name, :tiles, :regen_location, :music
+	attr_accessor :name, :tiles, :regen_location
 
 end
-
-# For manually testing this individual file (will need to comment out @tiles &@regen_locations in initialize)
-# @map = Map.new
-# @map.play_music(true)
-# @map.play_music(false)
