90e81e13f09bcd59d353c7fb9c1d4e593dcea6ad
Nicholas Skinsacos <nskins@umich.edu>
2017-04-18 22:16:31 -0400

    Refactor Attack & provide more tests

--- spec/Battle/BattleCommand/Attack/attack_spec.rb
+++ spec/Battle/BattleCommand/Attack/attack_spec.rb
@@ -4,6 +4,13 @@ require_relative '../../../../lib/Entity/Monster/monster.rb'
 
 RSpec.describe Attack do
 
+  before(:each) do
+    @user = Player.new(max_hp: 50, attack: 6, defense: 4)
+    @enemy = Monster.new(max_hp: 30, attack: 3, defense: 2)
+    @attack = Attack.new(strength: 5)
+    @cry = Attack.new(name: "Cry", success_rate: 0)
+  end
+
   context "constructor" do
     it "has the correct default parameters" do
       attack = Attack.new
@@ -20,7 +27,7 @@ RSpec.describe Attack do
       expect(poke.strength).to eq 12
       expect(poke.success_rate).to eq 95
     end
-    
+
     it "correctly assigns only one custom parameter" do
       attack = Attack.new(success_rate: 77)
       expect(attack.name).to eq "Attack"
@@ -40,26 +47,46 @@ RSpec.describe Attack do
       expect(poke).not_to eq kick
     end
   end
-  
+
   context "run" do
-    # TODO
+    it "does the appropriate amount of damage for attack > defense" do
+      @attack.run(@user, @enemy)
+      expect(@enemy.hp).to be_between(21, 24)
+    end
+
+    it "prevents the enemy's HP from falling below 0" do
+      @user.attack = 1000
+      @attack.run(@user, @enemy)
+      expect(@enemy.hp).to be_zero
+    end
+
+    it "does the appropriate amount of damage for defense > attack" do
+      @attack.run(@enemy, @user)
+      expect(@user.hp).to be_between(45, 46)
+    end
+
+    it "prints an appropriate message for a failed attack" do
+      expect { @cry.run(@user, @enemy) }.to output(
+        "Player tries to use Cry, but it fails.\n\n"
+      ).to_stdout
+    end
   end
 
   context "calculate damage" do
-    it "returns a random value" do
-      user = Player.new(name: "Hero",
-                        max_hp: 50,
-                        hp: 35,
-                        attack: 12,
-                        defense: 4)
+    it "returns within the appropriate range for attack > defense" do
+      damage = @attack.calculate_damage(@user, @enemy)
+      expect(damage).to be_between(6, 9)
+    end
 
-      enemy = Monster.new(max_hp: 30,
-                          defense: 2)
+    it "returns within the appropriate range for defense > attack" do
+      damage = @attack.calculate_damage(@enemy, @user)
+      expect(damage).to be_between(4, 5)
+    end
 
-      attack = Attack.new(strength: 5)
-      damage = attack.calculate_damage(user, enemy)
-      enemy.hp -= damage
-      expect(enemy.hp).to be_within(18).of(23)
+    it "defaults to 0 when the defense is very high" do
+      @enemy.defense = 100
+      damage = @attack.calculate_damage(@user, @enemy)
+      expect(damage).to be_zero
     end
   end
 
