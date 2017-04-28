d69ca5618c4357f921af7532b4e63240b3f76c9b
Nicholas Skinsacos <nskins@umich.edu>
2017-04-28 12:54:33 -0400

    Finish implementation of y/n responses (Fixes #53)

--- lib/extension.rb
+++ lib/extension.rb
@@ -1,12 +1,19 @@
 # Provides additional methods for String.
 class String
+
+  # Set of all known positive responses.
+  POSITIVE_RESPONSES = Set.new [ "ok", "okay", "sure", "y", "ye", "yeah", "yes" ]
+  # Set of all known negative responses.
+  NEGATIVE_RESPONSES = Set.new [ "n", "nah", "no", "nope" ]
+
   # Returns true iff the string is affirmative/positive.
   def is_positive?
-    true
+    return POSITIVE_RESPONSES.include?(self)
   end
 
   # Returns true iff the string is negatory/negative.
   def is_negative?
-    true
+    return NEGATIVE_RESPONSES.include?(self)
   end
+
 end
