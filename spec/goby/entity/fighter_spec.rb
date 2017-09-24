2be9482fec4fc45946d1089e16604252425a6bfa
Scott Nisbet <scott.nisbet@outlook.com>
2017-09-24 17:48:30 +1000

    Uplift fighter_spec tests

--- spec/goby/entity/fighter_spec.rb
+++ spec/goby/entity/fighter_spec.rb
@@ -2,8 +2,9 @@ require 'goby'
 
 RSpec.describe Fighter do
 
-  let!(:fighter) { Class.new { extend Fighter} }
-  let(:entity) { Entity.new.extend(Fighter) }
+  let!(:empty_fighter) { Class.new { extend Fighter } }
+  let(:fighter_class) { Class.new(Entity) { include Fighter } }
+  let(:fighter) { fighter_class.new }
 
   context "fighter" do
     it "is a fighter" do
@@ -13,34 +14,34 @@ RSpec.describe Fighter do
 
   context "placeholder methods" do
     it "forces :die to be implemented" do
-      expect { fighter.die }.to raise_error(NotImplementedError, 'A Fighter Entity must know how to die')
+      expect { empty_fighter.die }.to raise_error(NotImplementedError, 'A Fighter Entity must know how to die')
     end
 
     it "forces :sample_treasures to be implemented" do
-      expect { fighter.sample_treasures }.to raise_error(NotImplementedError, 'A Fighter Entity must know whether it returns treasure or not after losing a battle')
+      expect { empty_fighter.sample_treasures }.to raise_error(NotImplementedError, 'A Fighter Entity must know whether it returns treasure or not after losing a battle')
     end
 
     it "forces :sample_gold to be implemented" do
-      expect { fighter.sample_gold }.to raise_error(NotImplementedError, 'A Fighter Entity must return some gold after losing a battle')
+      expect { empty_fighter.sample_gold }.to raise_error(NotImplementedError, 'A Fighter Entity must return some gold after losing a battle')
     end
   end
 
   context "add battle command" do
     it "properly adds the command in a trivial case" do
-      entity.add_battle_command(BattleCommand.new)
-      expect(entity.battle_commands.length).to eq 1
-      expect(entity.battle_commands).to eq [BattleCommand.new]
+      fighter.add_battle_command(BattleCommand.new)
+      expect(fighter.battle_commands.length).to eq 1
+      expect(fighter.battle_commands).to eq [BattleCommand.new]
     end
 
     it "maintains the sorted invariant for a more complex case" do
-      entity.add_battle_command(BattleCommand.new(name: "Kick"))
-      entity.add_battle_command(BattleCommand.new(name: "Chop"))
-      entity.add_battle_command(BattleCommand.new(name: "Grab"))
-      expect(entity.battle_commands.length).to eq 3
-      expect(entity.battle_commands).to eq [
-                                               BattleCommand.new(name: "Chop"),
-                                               BattleCommand.new(name: "Grab"),
-                                               BattleCommand.new(name: "Kick")]
+      fighter.add_battle_command(BattleCommand.new(name: "Kick"))
+      fighter.add_battle_command(BattleCommand.new(name: "Chop"))
+      fighter.add_battle_command(BattleCommand.new(name: "Grab"))
+      expect(fighter.battle_commands.length).to eq 3
+      expect(fighter.battle_commands).to eq [
+                                                BattleCommand.new(name: "Chop"),
+                                                BattleCommand.new(name: "Grab"),
+                                                BattleCommand.new(name: "Kick")]
     end
   end
 
@@ -48,7 +49,7 @@ RSpec.describe Fighter do
     it "randomly selects one of the available commands" do
       kick = BattleCommand.new(name: "Kick")
       zap = BattleCommand.new(name: "Zap")
-      entity = Entity.new(battle_commands: [kick, zap]).extend(Fighter)
+      entity = fighter_class.new(battle_commands: [kick, zap])
       attack = entity.choose_attack
       expect(attack.name).to eq("Kick").or(eq("Zap"))
     end
@@ -59,9 +60,9 @@ RSpec.describe Fighter do
       banana = Item.new(name: "Banana")
       axe = Item.new(name: "Axe")
 
-      entity = Entity.new(inventory: [C[banana, 1],
-                                      C[axe, 3]]).extend(Fighter)
-      enemy = Entity.new(name: "Enemy").extend(Fighter)
+      entity = fighter_class.new(inventory: [C[banana, 1],
+                                       C[axe, 3]])
+      enemy = fighter_class.new(name: "Enemy")
 
       pair = entity.choose_item_and_on_whom(enemy)
       expect(pair.first.name).to eq("Banana").or(eq("Axe"))
@@ -69,81 +70,81 @@ RSpec.describe Fighter do
     end
   end
 
-  context "eqip item" do
+  context "equip item" do
     it "correctly equips the weapon and alters the stats of a Fighter Entity" do
-      entity = Entity.new(inventory: [C[
-                                          Weapon.new(stat_change: { attack: 3 },
-                                                     attack: Attack.new), 1]]).extend(Fighter)
+      entity = fighter_class.new(inventory: [C[
+                                           Weapon.new(stat_change: {attack: 3},
+                                                      attack: Attack.new), 1]])
       entity.equip_item("Weapon")
       expect(entity.outfit[:weapon]).to eq Weapon.new
       expect(entity.stats[:attack]).to eq 4
       expect(entity.battle_commands).to eq [Attack.new]
     end
 
-    # it "correctly switches the equipped items and alters status as appropriate" do
-    #   entity = Entity.new(inventory: [C[
-    #                                       Weapon.new(name: "Hammer",
-    #                                                  stat_change: { attack: 3,
-    #                                                                 defense: 2,
-    #                                                                 agility: 4 },
-    #                                                  attack: Attack.new(name: "Bash")), 1],
-    #                                   C[
-    #                                       Weapon.new(name: "Knife",
-    #                                                  stat_change: { attack: 5,
-    #                                                                 defense: 3,
-    #                                                                 agility: 7 },
-    #                                                  attack: Attack.new(name: "Stab")), 1]])
-    #   entity.equip_item("Hammer")
-    #   stats = entity.stats
-    #   expect(stats[:attack]).to eq 4
-    #   expect(stats[:defense]).to eq 3
-    #   expect(stats[:agility]).to eq 5
-    #   expect(entity.outfit[:weapon].name).to eq "Hammer"
-    #   expect(entity.battle_commands).to eq [Attack.new(name: "Bash")]
-    #   expect(entity.inventory.length).to eq 1
-    #   expect(entity.inventory[0].first.name).to eq "Knife"
-    #
-    #   entity.equip_item("Knife")
-    #   stats = entity.stats
-    #   expect(stats[:attack]).to eq 6
-    #   expect(stats[:defense]).to eq 4
-    #   expect(stats[:agility]).to eq 8
-    #   expect(entity.outfit[:weapon].name).to eq "Knife"
-    #   expect(entity.battle_commands).to eq [Attack.new(name: "Stab")]
-    #   expect(entity.inventory.length).to eq 1
-    #   expect(entity.inventory[0].first.name).to eq "Hammer"
-    # end
+    it "correctly switches the equipped items and alters status of a Fighter Entity as appropriate" do
+      entity = fighter_class.new(inventory: [C[
+                                          Weapon.new(name: "Hammer",
+                                                     stat_change: { attack: 3,
+                                                                    defense: 2,
+                                                                    agility: 4 },
+                                                     attack: Attack.new(name: "Bash")), 1],
+                                      C[
+                                          Weapon.new(name: "Knife",
+                                                     stat_change: { attack: 5,
+                                                                    defense: 3,
+                                                                    agility: 7 },
+                                                     attack: Attack.new(name: "Stab")), 1]])
+      entity.equip_item("Hammer")
+      stats = entity.stats
+      expect(stats[:attack]).to eq 4
+      expect(stats[:defense]).to eq 3
+      expect(stats[:agility]).to eq 5
+      expect(entity.outfit[:weapon].name).to eq "Hammer"
+      expect(entity.battle_commands).to eq [Attack.new(name: "Bash")]
+      expect(entity.inventory.length).to eq 1
+      expect(entity.inventory[0].first.name).to eq "Knife"
+
+      entity.equip_item("Knife")
+      stats = entity.stats
+      expect(stats[:attack]).to eq 6
+      expect(stats[:defense]).to eq 4
+      expect(stats[:agility]).to eq 8
+      expect(entity.outfit[:weapon].name).to eq "Knife"
+      expect(entity.battle_commands).to eq [Attack.new(name: "Stab")]
+      expect(entity.inventory.length).to eq 1
+      expect(entity.inventory[0].first.name).to eq "Hammer"
+    end
   end
 
   context "has battle command" do
     it "correctly indicates an absent command for an object argument" do
-      entity = Entity.new(battle_commands: [
+      entity = fighter_class.new(battle_commands: [
                               BattleCommand.new(name: "Kick"),
-                              BattleCommand.new(name: "Poke")]).extend(Fighter)
+                              BattleCommand.new(name: "Poke")])
       index = entity.has_battle_command(BattleCommand.new(name: "Chop"))
       expect(index).to be_nil
     end
 
     it "correctly indicates a present command for an object argument" do
-      entity = Entity.new(battle_commands: [
+      entity = fighter_class.new(battle_commands: [
                               BattleCommand.new(name: "Kick"),
-                              BattleCommand.new(name: "Poke")]).extend(Fighter)
+                              BattleCommand.new(name: "Poke")])
       index = entity.has_battle_command(BattleCommand.new(name: "Poke"))
       expect(index).to eq 1
     end
 
     it "correctly indicates an absent command for a string argument" do
-      entity = Entity.new(battle_commands: [
+      entity = fighter_class.new(battle_commands: [
                               BattleCommand.new(name: "Kick"),
-                              BattleCommand.new(name: "Poke")]).extend(Fighter)
+                              BattleCommand.new(name: "Poke")])
       index = entity.has_battle_command("Chop")
       expect(index).to be_nil
     end
 
     it "correctly indicates a present command for a string argument" do
-      entity = Entity.new(battle_commands: [
+      entity = fighter_class.new(battle_commands: [
                               BattleCommand.new(name: "Kick"),
-                              BattleCommand.new(name: "Poke")]).extend(Fighter)
+                              BattleCommand.new(name: "Poke")])
       index = entity.has_battle_command("Poke")
       expect(index).to eq 1
     end
@@ -151,15 +152,15 @@ RSpec.describe Fighter do
 
   context "remove battle command" do
     it "has no effect when no such command is present" do
-      entity.add_battle_command(Attack.new(name: "Kick"))
-      entity.remove_battle_command(BattleCommand.new(name: "Poke"))
-      expect(entity.battle_commands.length).to eq 1
+      fighter.add_battle_command(Attack.new(name: "Kick"))
+      fighter.remove_battle_command(BattleCommand.new(name: "Poke"))
+      expect(fighter.battle_commands.length).to eq 1
     end
 
     it "correctly removes the command in the trivial case" do
-      entity.add_battle_command(Attack.new(name: "Kick"))
-      entity.remove_battle_command(Attack.new(name: "Kick"))
-      expect(entity.battle_commands.length).to eq 0
+      fighter.add_battle_command(Attack.new(name: "Kick"))
+      fighter.remove_battle_command(Attack.new(name: "Kick"))
+      expect(fighter.battle_commands.length).to eq 0
     end
   end
 
