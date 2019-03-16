a9744eb7bb0435f3fac4f07420e4dd81b3651163
Nitish Rathi <nitishrathi@gmail.com>
2019-03-16 21:40:23 +0000

    rubocop improvements

--- lib/goby/battle/battle.rb
+++ lib/goby/battle/battle.rb
@@ -1,10 +1,8 @@
 require 'goby'
 
 module Goby
-
   # Representation of a fight between two Fighters.
   class Battle
-
     # @param [Entity] entity_a the first entity in the battle
     # @param [Entity] entity_b the second entity in the battle
     def initialize(entity_a, entity_b)
@@ -19,17 +17,17 @@ module Goby
     def determine_winner
       type("#{entity_a.name} enters a battle with #{entity_b.name}!\n\n")
       until someone_dead?
-        #Determine order of attacks
+        # Determine order of attacks
         attackers = determine_order
 
         # Both choose an attack.
-        attacks = attackers.map { |attacker| attacker.choose_attack }
+        attacks = attackers.map(&:choose_attack)
 
         2.times do |i|
           # The attacker runs its attack on the other attacker.
           attacks[i].run(attackers[i], attackers[(i + 1) % 2])
 
-          if (attackers[i].escaped)
+          if attackers[i].escaped
             attackers[i].escaped = false
             return
           end
@@ -63,5 +61,4 @@ module Goby
 
     attr_reader :entity_a, :entity_b
   end
-
 end
