ba41aa729a19b5610d18b3ac0a7567c43f8d6ff9
Nitish Rathi <nitishrathi@gmail.com>
2019-03-16 23:25:25 +0000

    rubocop autocorrect fixes

--- lib/goby/battle/escape.rb
+++ lib/goby/battle/escape.rb
@@ -1,18 +1,16 @@
 require 'goby'
 
 module Goby
-
   # Allows an Entity to try to escape from the opponent.
   class Escape < BattleCommand
-
     # Text for successful escape.
-    SUCCESS = "Successful escape!\n\n"
+    SUCCESS = "Successful escape!\n\n".freeze
     # Text for failed escape.
-    FAILURE = "Unable to escape!\n\n"
+    FAILURE = "Unable to escape!\n\n".freeze
 
     # Initializes the Escape command.
     def initialize
-      super(name: "Escape")
+      super(name: 'Escape')
     end
 
     # Samples a probability to determine if the user will escape from battle.
@@ -20,9 +18,8 @@ module Goby
     # @param [Entity] user the one who is trying to escape.
     # @param [Entity] enemy the one from whom the user wants to escape.
     def run(user, enemy)
-
       # Higher probability of escape when the enemy has low agility.
-      if (user.sample_agilities(enemy))
+      if user.sample_agilities(enemy)
         user.escaped = true
         type(SUCCESS)
         return
@@ -32,7 +29,5 @@ module Goby
       user.escaped = false
       type(FAILURE)
     end
-
   end
-
 end
