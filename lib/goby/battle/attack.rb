19b853ba979dde8fd1f92748a3622fd77ff6de12
Nitish Rathi <nitishrathi@gmail.com>
2019-03-16 17:55:32 +0000

    rubocop autocorrect & unused assignment

--- lib/goby/battle/attack.rb
+++ lib/goby/battle/attack.rb
@@ -1,14 +1,12 @@
 require 'goby'
 
 module Goby
-
   # Simple physical attack.
   class Attack < BattleCommand
-
     # @param [String] name the name.
     # @param [Integer] strength the strength.
     # @param [Integer] success_rate the chance of success.
-    def initialize(name: "Attack", strength: 1, success_rate: 100)
+    def initialize(name: 'Attack', strength: 1, success_rate: 100)
       super(name: name)
       @strength = strength
       @success_rate = success_rate
@@ -20,10 +18,8 @@ module Goby
     # @param [Entity] enemy the one on whom the attack is used.
     # @return [Integer] the amount of damage to inflict on the enemy.
     def calculate_damage(user, enemy)
-
       # RANDOMIZE ATTACK
       inflict = Random.rand(0.05..0.15).round(2)
-      multiplier = 1
 
       if enemy.stats[:defense] > user.stats[:attack]
         multiplier = 1 - ((enemy.stats[:defense] * 0.1) - (user.stats[:attack] * inflict))
@@ -35,7 +31,7 @@ module Goby
         multiplier = 1 + ((user.stats[:attack] * inflict) - (enemy.stats[:defense] * 0.1))
       end
 
-      return (@strength * multiplier).round(0)
+      (@strength * multiplier).round(0)
     end
 
     # Inflicts damage on the enemy and prints output.
@@ -43,7 +39,7 @@ module Goby
     # @param [Entity] user the one who is using the attack.
     # @param [Entity] enemy the one on whom the attack is used.
     def run(user, enemy)
-      if (Random.rand(100) < @success_rate)
+      if Random.rand(100) < @success_rate
 
         # Damage the enemy.
         original_enemy_hp = enemy.stats[:hp]
@@ -58,11 +54,8 @@ module Goby
       else
         type("#{user.name} tries to use #{@name}, but it fails.\n\n")
       end
-
     end
 
     attr_accessor :strength, :success_rate
-
   end
-
 end
