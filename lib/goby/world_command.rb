4dd40d3d8067165c9b1676bfc8550fcb178c0391
Nitish Rathi <nitishrathi@gmail.com>
2019-03-16 17:39:19 +0000

    extract method to DRY

--- lib/goby/world_command.rb
+++ lib/goby/world_command.rb
@@ -30,7 +30,7 @@ module Goby
     #
     # @param [Player] player the player who wants to see the special commands.
     def display_special_commands(player)
-      commands = player.tile.events.select(&:visible).map(&:command)
+      commands = visible_events(player).map(&:command)
       print SPECIAL_COMMANDS_HEADER + commands.join(', ') + "\n\n" if commands.any?
     end
 
@@ -82,8 +82,7 @@ module Goby
                            'save' => -> { save_game(player, 'player.yaml') }
                          }
                        end
-      event_commands = player
-                       .tile.events.select(&:visible)
+      event_commands = visible_events(player)
                        .map { |event| [event.command, -> { event.run(player) }] }
       _cmd, action = [*fixed_commands, *event_commands]
                      .detect { |cmd, _action| keyword.casecmp?(cmd) }
@@ -93,6 +92,12 @@ module Goby
       # TODO: Add string literal for this.
       puts "That isn't an available command at this time."
       print "Type 'help' for a list of available commands.\n\n"
+    end
+
+    private
+
+    def visible_events(player)
+      player.tile.events.select(&:visible)
     end
   end
 end
