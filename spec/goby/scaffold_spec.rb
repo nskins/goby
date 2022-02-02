ed78d254d5ffa6bbcc3fbc53e83b9cb31bc22948
Lily <lolpo12345@outlook.com>
2022-02-02 22:25:40 +0300

    Rewrite the Scaffold tests to fit the new project creation methods

--- spec/goby/scaffold_spec.rb
+++ spec/goby/scaffold_spec.rb
@@ -4,26 +4,23 @@ require 'fileutils'
 RSpec.describe Scaffold do
 
   context "simple" do
-    it "should create the appropriate directories & files" do
+    it "should create the appropriate directories & files in the current directory" do
 
-      project = "goby-project"
-      Scaffold::simple project
+      Scaffold::simple
 
       # Ensure all of the directories exist.
-      expect(Dir.exists? "#{project}").to be true
       [ '', 'battle', 'entity',
       'event', 'item', 'map' ].each do |dir|
-        expect(Dir.exist? "#{project}/src/#{dir}").to be true
+        expect(Dir.exist? "src/#{dir}").to be true
       end
 
       # Ensure all of the files exist.
       [ '.gitignore', 'src/main.rb', 'src/map/farm.rb' ].each do |file|
-        expect(File.exist? "#{project}/#{file}").to be true
+        expect(File.exist? "#{file}").to be true
       end
 
       # Clean up the scaffolding.
-      FileUtils.remove_dir project
-
+      FileUtils.remove_dir "src"
     end
   end
 
